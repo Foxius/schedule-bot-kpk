a21fd5e515b709ca7be561dc4dc150f250f2f188
saikonohack <81900523+Foxius@users.noreply.github.com>
2022-08-13 19:52:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,36 +2,59 @@ import openpyxl
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InputMediaDocument
 from telebot import types
-
-print("""Доброе утро, семпай)""")
+from year20 import *
+from year21 import *
+from year22 import *
+print("""Охаё, семпай)""")
 print("""Приступаю к работе""")
-
-bot = telebot.TeleBot('5597120964:AAEyTnVfB4wj-mL1ekygTCDnUygJ_MXEhNQ')
+########################################################################
+bot = telebot.TeleBot('5934523592:YBubuBJHBubyUTBVUIBugioduibyuIbHBNK')#
+########################################################################
 book = openpyxl.open("data.xlsx", read_only=True)
 sheet = book.active
 
 @bot.message_handler(commands=['start'])
 def start(message):
 	keyboard=types.ReplyKeyboardMarkup(resize_keyboard=True)
-	btn = types.KeyboardButton("МРОА-20")
-	keyboard.add(btn)
-	btn1 = types.KeyboardButton("С-20")
-	keyboard.add(btn1)
-	btn2 = types.KeyboardButton("Т-20")
-	keyboard.add(btn2)
-	btn3 = types.KeyboardButton("МЖКХ-20")
-	keyboard.add(btn3)
+	mroa20 = types.KeyboardButton("МРОА-20")
+	s20 = types.KeyboardButton("С-20")
+	t20 = types.KeyboardButton("Т-20")
+	mjkh20 = types.KeyboardButton("МЖКХ-20")
+	keyboard.row(mroa20, s20, t20, mjkh20)
+	mci20 = types.KeyboardButton("МЦИ-20")
+	at2_20 = types.KeyboardButton("2АТ-20")
+	sp20 = types.KeyboardButton("СП-20")
+	k20 = types.KeyboardButton("К-20")
+	keyboard.row(mci20, at2_20, sp20, k20)
+	mroa21 = types.KeyboardButton("МРОА-21")
+	s21 = types.KeyboardButton("С-21")
+	t21 = types.KeyboardButton("Т-21")
+	mjkh21 = types.KeyboardButton("МЖКХ-21")
+	keyboard.row(mroa21, s21, t21, mjkh21)
+	mci21 = types.KeyboardButton("МЦИ-21")
+	at2_21 = types.KeyboardButton("2АТ-21")
+	sp21 = types.KeyboardButton("СП-21")
+	k21 = types.KeyboardButton("К-21")
+	keyboard.row(mci21, at2_21, sp21, k21)
+	mroa22 = types.KeyboardButton("МРОА-22")
+	s22 = types.KeyboardButton("С-22")
+	t22 = types.KeyboardButton("Т-22")
+	mjkh22 = types.KeyboardButton("МЖКХ-22")
+	keyboard.row(mroa22, s22, t22, mjkh22)
+	mci22 = types.KeyboardButton("МЦИ-22")
+	at2_22 = types.KeyboardButton("2АТ-22")
+	sp22 = types.KeyboardButton("СП-22")
+	k22 = types.KeyboardButton("К-22")
+	keyboard.row(mci22, at2_22, sp22, k22)
 	mainmessage = '''Приветик) Меня зовут *Распи*. Я подскажу тебе расписание твоих пар на сегодня в твоём любимом Копейском Политехническом.)
 	
-	Чтобы я тебе помогала и дальше, запомни: Перед получением расписания *ОБЯЗАТЕЛЬНО* поздоровайся со мной при помощи команды /start
-	
-	И только после этого выбирай свою группу. Удачного дня, семпай)
+	Выбирай свою группу. Удачного дня, семпай)
 	'''
 	photo = open('photomes.jpg', 'rb')
 	mainmesage = bot.send_photo(message.chat.id, photo, caption=mainmessage, parse_mode="Markdown", reply_markup=keyboard)
-	bot.register_next_step_handler(mainmesage,mainfunction)
 
 
+@bot.message_handler(content_types=['text'])
 def mainfunction(message):
 	if message.text == 'МРОА-20':
 		mroa20(message)
@@ -39,81 +62,50 @@ def mainfunction(message):
 		s20(message)
 	if message.text == 'Т-20':	
 		t20(message)
-	if message.text == 'МЖКХ-20':	
-		mjkh20(message)	
-	else:
-		bot.send_message(message.chat.id, text="Произошла какая-то ошибка :( попробуй еще раз, пожалуйста")
-
-def mroa20(message):
-	para1 = sheet["A2"].value
-	paracab1 = sheet["B2"].value
-	para2 = sheet["A3"].value
-	paracab2 = sheet["B3"].value
-	para3 = sheet["A4"].value
-	paracab3 = sheet["B4"].value
-	para4 = sheet["A5"].value
-	paracab4 = sheet["B5"].value
-	bot.send_message(message.chat.id, f'''*Расписание на сегодня для группы* `МРОА-20`: 
-		*1.* {para1} - `{paracab1}`
-		*2.* {para2} - `{paracab2}`
-		*3.* {para3} - `{paracab3}`
-		*4.* {para4} - `{paracab4}`''', parse_mode='Markdown',reply_markup=types.ReplyKeyboardRemove())
-
-def s20(message):
-	para1 = sheet["C2"].value
-	paracab1 = sheet["D2"].value
-	para2 = sheet["C3"].value
-	paracab2 = sheet["D3"].value
-	para3 = sheet["C4"].value
-	paracab3 = sheet["D4"].value
-	para4 = sheet["C5"].value
-	paracab4 = sheet["D5"].value
-	bot.send_message(message.chat.id, f'''*Расписание на сегодня для группы* `С-20`:
-		*1.* {para1} - `{paracab1}`
-		*2.* {para2} - `{paracab2}`
-		*3.* {para3} - `{paracab3}`
-		*4.* {para4} - `{paracab4}`''', parse_mode='Markdown',reply_markup=types.ReplyKeyboardRemove())
-
-def t20(message):
-	para1 = sheet["E2"].value
-	paracab1 = sheet["F2"].value
-	para2 = sheet["E3"].value
-	paracab2 = sheet["F3"].value
-	para3 = sheet["E4"].value
-	paracab3 = sheet["F4"].value
-	para4 = sheet["E5"].value
-	paracab4 = sheet["F5"].value
-	bot.send_message(message.chat.id, f'''*Расписание на сегодня для группы* `Т-20`:
-		*1.* {para1} - `{paracab1}`
-		*2.* {para2} - `{paracab2}`
-		*3.* {para3} - `{paracab3}`
-		*4.* {para4} - `{paracab4}`''', parse_mode='Markdown',reply_markup=types.ReplyKeyboardRemove())
-
-def mjkh20(message):
-	para1 = sheet["G2"].value
-	paracab1 = sheet["H2"].value
-	para2 = sheet["G3"].value
-	paracab2 = sheet["H3"].value
-	para3 = sheet["G4"].value
-	paracab3 = sheet["H4"].value
-	para4 = sheet["G5"].value
-	paracab4 = sheet["H5"].value
-	bot.send_message(message.chat.id, f'''*Расписание на сегодня для группы* `МЖКХ-20`:
-		*1.* {para1} - `{paracab1}`
-		*2.* {para2} - `{paracab2}`
-		*3.* {para3} - `{paracab3}`
-		*4.* {para4} - `{paracab4}`''', parse_mode='Markdown',reply_markup=types.ReplyKeyboardRemove())
-
-
-
-
-
-
-
-
-
-
-
+	if message.text == 'МЦИ-20':	
+		mci20(message)
+	if message.text == '2АТ-20':	
+		at2_20(message)
+	if message.text == 'СП-20':	
+		sp20(message)
+	if message.text == 'МЖКХ-20':
+		mjkh20(message)
+	if message.text == 'К-20':	
+		k20(message)
+	if message.text == 'МРОА-21':
+		mroa21(message)
+	if message.text == 'С-21':
+		s21(message)
+	if message.text == 'Т-21':	
+		t21(message)
+	if message.text == 'МЦИ-21':	
+		mci21(message)
+	if message.text == '2АТ-21':	
+		at2_21(message)
+	if message.text == 'СП-21':	
+		sp21(message)
+	if message.text == 'МЖКХ-21':
+		mjkh21(message)
+	if message.text == 'К-21':	
+		k21(message)
+	if message.text == 'МРОА-22':
+		mroa22(message)
+	if message.text == 'С-22':
+		s22(message)
+	if message.text == 'Т-22':	
+		t22(message)
+	if message.text == 'МЦИ-22':	
+		mci22(message)
+	if message.text == '2АТ-22':	
+		at2_22(message)
+	if message.text == 'СП-22':	
+		sp22(message)
+	if message.text == 'МЖКХ-22':
+		mjkh22(message)
+	if message.text == 'К-22':	
+		k22(message)
+	# else:
+	# 	bot.send_message(message.chat.id, text="Произошла какая-то ошибка :( попробуй еще раз, пожалуйста")
 
 
 @bot.message_handler(commands=['author'])
@@ -127,12 +119,20 @@ def author(message):
 	"""
 	bot.send_photo(message.chat.id, photo, caption=authormessage, parse_mode='Markdown',reply_markup=types.ReplyKeyboardRemove())
 
+@bot.message_handler(commands=['help'])
+def help(message):
+	pass
+@bot.message_handler(commands=['call'])
+def call(message):
+	bot.send_message(message.chat.id, text="""*Расписание звонков*
+		1) 8:00 - 8:45, 8:55 - 9:40
+		2) 9:50 - 10:30, 10:55 - 11:40
+		3) 12:00 - 12:45, 12:55 - 13:40
+		4) 13:50 - 14:35, 14:45 - 15:30
+		""", parse_mode='Markdown')
 
 bot.polling(none_stop=True, interval=0)
 try:
     bot.polling(none_stop=True)
 except Exception as e:
     logger.exception("Fail startup:", e)
-
-
-# print(sheet["B2"].value)
